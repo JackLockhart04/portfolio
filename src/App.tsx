f89a903220ac6b73ed5b5cb07154ac613a8013ca
Jack Lockhart <jrlockhart2@crimson.ua.edu>
2024-11-05 19:32:00 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.tsx
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+import Navbar from "./components/navbar/navbar";
+import Home from "./pages/homePage/home";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />}>
+          <Route path="team" element={<Team />} />
+          <Route path="history" element={<History />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./navbar.css";
+
+const Navbar: React.FC = () => {
+  return <nav className="navbar"></nav>;
+};
+
+export default Navbar;
