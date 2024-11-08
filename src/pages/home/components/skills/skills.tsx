19f9cfdacae27a8910d17b63bf78a50dc12a92a1
Jack Lockhart <jrlockhart2@crimson.ua.edu>
2024-11-08 19:22:11 +0000

    Updated skills mostly

--- src/pages/home/components/skills/skills.tsx
+++ src/pages/home/components/skills/skills.tsx
@@ -1,71 +1,54 @@
 import React from "react";
-
 import "./skills.css";
 
 const Skills: React.FC = () => {
   return (
     <div className="homeSkillsSection">
       <h2 className="homeSkillsHeader">Skills</h2>
-      <div className="homeSkillsCategory">
-        <h3>Frontend</h3>
-        <div className="homeSkillsGrid">
-          <div className="homeSkillItem">
+      <div className="homeSkillsGroupContainer">
+        <div className="homeSkillsGroup">
+          <div className="homeSkillsGroupHeader">
             <img
-              src={`${process.env.PUBLIC_URL}/logos/javascript-logo.png`}
-              alt="JavaScript"
-              className="homeSkillLogo"
+              src={`${process.env.PUBLIC_URL}/logos/frontend-icon.png`}
+              alt="Frontend"
+              className="homeSkillsGroupIcon"
             />
+            <h3>Frontend</h3>
+          </div>
+          <div className="homeSkillsList">
             <span>JavaScript</span>
-          </div>
-          <div className="homeSkillItem">
-            <img
-              src={`${process.env.PUBLIC_URL}/logos/typescript-logo.png`}
-              alt="TypeScript"
-              className="homeSkillLogo"
-            />
             <span>TypeScript</span>
-          </div>
-          <div className="homeSkillItem">
-            <img
-              src={`${process.env.PUBLIC_URL}/logos/react-logo.png`}
-              alt="React"
-              className="homeSkillLogo"
-            />
             <span>React</span>
           </div>
         </div>
-      </div>
-      <div className="homeSkillsCategory">
-        <h3>Backend</h3>
-        <div className="homeSkillsGrid">
-          <div className="homeSkillItem">
+        <div className="homeSkillsGroup">
+          <div className="homeSkillsGroupHeader">
             <img
-              src={`${process.env.PUBLIC_URL}/logos/C++-logo.png`}
-              alt="C++"
-              className="homeSkillLogo"
+              src={`${process.env.PUBLIC_URL}/logos/backend-icon.png`}
+              alt="Backend"
+              className="homeSkillsGroupIcon"
             />
-            <span>C++</span>
+            <h3>Backend</h3>
           </div>
-          <div className="homeSkillItem">
-            <img
-              src={`${process.env.PUBLIC_URL}/logos/python-logo.png`}
-              alt="Python"
-              className="homeSkillLogo"
-            />
+          <div className="homeSkillsList">
+            <span>C++</span>
             <span>Python</span>
           </div>
         </div>
-      </div>
-      <div className="homeSkillsCategory">
-        <h3>Cloud Computing</h3>
-        <div className="homeSkillsGrid">
-          <div className="homeSkillItem">
+        <div className="homeSkillsGroup">
+          <div className="homeSkillsGroupHeader">
             <img
-              src={`${process.env.PUBLIC_URL}/logos/aws-logo.png`}
-              alt="AWS"
-              className="homeSkillLogo"
+              src={`${process.env.PUBLIC_URL}/logos/other-icon.png`}
+              alt="Cloud Computing"
+              className="homeSkillsGroupIcon"
             />
+            <h3>Other Technologies</h3>
+          </div>
+          <div className="homeSkillsList">
+            <span>Git</span>
             <span>AWS</span>
+            <span>NoSQL</span>
+            <span>SQL</span>
           </div>
         </div>
       </div>
--- src/pages/stocksInSeconds/stocksInSeconds.tsx
+++ src/pages/stocksInSeconds/stocksInSeconds.tsx
@@ -3,7 +3,7 @@ import "./stocksInSeconds.css";
 
 const StocksInSeconds: React.FC = () => {
   return (
-    <div className="mainContainer">
+    <div className="stocksInSecondsContainer">
       <h1 className="title">StocksInSeconds.com</h1>
       <div className="imageSection">
         <img
@@ -12,7 +12,25 @@ const StocksInSeconds: React.FC = () => {
           className="projectImage"
         />
       </div>
-      <div className="descriptionContainer"></div>
+      <div className="linkSection">
+        <a
+          href="https://www.StocksInSeconds.com/"
+          className="projectLink"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit stocksinseconds.com
+        </a>
+      </div>
+      <div className="descriptionContainer">
+        <p>
+          StocksInSeconds.com is a website for a startup company. I developed
+          the backend along with the communications on the frontend. I was able
+          to use my knowledge of python and javascript but I had to learn many
+          technologies for this project such as Flask API, some AWS, NoSQL, user
+          authentication and authorization, cookies, domains, and more.
+        </p>
+      </div>
     </div>
   );
 };
